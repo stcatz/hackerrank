8eb2df813f252bb5eee5a279ea1866a9118594fc
stcatz <stcatzlee@gmail.com>
2014-12-25 15:48:29 +0800

    modify the advanced one

--- arrays_and_sorting/sherlock-and-queries/main.go
+++ arrays_and_sorting/sherlock-and-queries/main.go
@@ -9,9 +9,10 @@ func main() {
 	var n, m int
 	fmt.Scanf("%d %d\n", &n, &m)
 
-	a :=  make([]uint64, n)
-	b :=  make([]uint64, m)
-	c :=  make([]uint64, m)
+	a :=  make([]int64, n)
+	mark := make([]int64, n)
+	b :=  make([]int64, m)
+	c :=  make([]int64, m)
 	in := bufio.NewReader(os.Stdin)
 	line, _ := in.ReadString('\n')
 	line = line[:len(line)-1]
@@ -20,7 +21,9 @@ func main() {
 
 	for i:=0; i<n; i++ {
         temp, _ := strconv.Atoi(splited[i])
-        a[i] = uint64(temp)
+        a[i] = int64(temp)
+        mark[i] = -1
+
 	}
 
 	line, _ = in.ReadString('\n')
@@ -29,27 +32,34 @@ func main() {
 
 	for i:=0; i<m; i++ {
         temp, _ := strconv.Atoi(splited[i])
-        b[i] = uint64(temp)
+        b[i] = int64(temp)
 	}
 
 	line, _ = in.ReadString('\n')
+
 	splited = strings.Split(line, " ")
 
 	for i:=0; i<m; i++ {
         temp, _ := strconv.Atoi(splited[i])
-        c[i] = uint64(temp)
+        c[i] = int64(temp)
+	}
+
+	for i := 0; i < m; i++ {
+		if(mark[b[i]-1] == -1){
+			mark[b[i]-1] = c[i]
+		}else{
+			mark[b[i]-1] = (mark[b[i]-1] * c[i]) % 1000000007
+		}
 	}
 
 
-	for i := 0; i < m; i++ {
-		c[i] = c[i] % 1000000007
-		factor := b[i]
-		for j:=1; ;j++{
-			index := factor * uint64(j)
-			if index > uint64(n) {
-				break
+
+	for i := 1; i <= n; i++ {
+		for j := 1; (j * i) <=n ; j++ {
+			if mark[i-1] != -1{
+				a[i*j-1] = (a[i*j-1] * mark[i-1]) % 1000000007
 			}
-			a[index-1] = (a[index-1] * c[i])% 1000000007
+			
 		}
 		
 	}
